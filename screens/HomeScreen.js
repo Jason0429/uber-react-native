b608f3c3c1a6fceae08554317c9206526cc42569
Jason Cheung <jasoncheung0429@gmail.com>
2021-11-13 22:20:46 -0500

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = () => {
+	return (
+		<SafeAreaView>
+			<Text>Home Screen</Text>
+		</SafeAreaView>
+	);
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
